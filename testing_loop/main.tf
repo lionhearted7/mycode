3d82f8cf0fc9d8540223f54beae2c61c31c19f43
lionhearted7 <lionhearted77709@gmail.com>
2023-03-14 20:12:18 +0000

    testing for_each loop

---
+++ testing_loop/main.tf
@@ -0,0 +1,47 @@
+/* Alta3 Research - rzfeeser@alta3.com
+   Working with "for_each" within a null_resource */
+
+/* Terraform block */
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+/* provider block */
+provider "null" {
+  # Configuration options
+}
+
+/* a list of local variables */
+
+locals {
+  Machines = [{ "RAM" = "5Gi", "CPU" = "5", "Name" = "alpha" }, { "RAM" = "3Gi", "CPU" = "3", "Name" = "bravo" }]
+}
+
+/* The null_resource implements the standard resource lifecycle but takes no more action */
+resource "null_resource" "Machines" {
+  for_each = toset(local.Machines)
+  /* triggers allows specifying a random set of values that when
+     changed will cause the resource to be replaced */
+  triggers = {
+    # name = each.value  // a special variable, "each" created by terraform
+    // the object has "each.key" and "each.value"
+    # CPU count = each.value.CPU
+    # RAM count = each.value.RAM
+    # Name count = each.value.Name
+
+    Resource_name  = each.key
+    resource_value = each.value
+
+  }
+}
+
+/* We want these outputs */
+output "Machines" {
+  value = null_resource.Machines
+}
+
